d4e1e9cd56ec2a8ab8831c3db8022864ca7302f4
Bushido-brown <orizut@gmail.com>
2022-08-11 17:46:02 -0700

    CREATE MULTIPLE TABLES AND ADD COMPLEX QUERIES

--- data.sql
+++ data.sql
@@ -11,3 +11,32 @@
  INSERT INTO animals VALUES (9, 'Boarmon', '2005-06-07', 7, true, 20.4);
  INSERT INTO animals VALUES (10, 'Blossom', '1998-10-13', 3, true, 17);
  INSERT INTO animals VALUES (11, 'Ditto', '2022-05-14', 4, true, 22);
+
+ --Owners table
+
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith' , 34); 
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell' , 19); 
+INSERT INTO owners (full_name, age) VALUES ('Bob' , 45); 
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond ' , 77); 
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester' , 14); 
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker' , 38);  
+
+/*
+** species table
+*/
+
+INSERT INTO species (name) VALUES ('Pokemon'); 
+INSERT INTO species (name) VALUES ('Digimon'); 
+
+-- modify animals table with species_id
+
+UPDATE animals SET species_id = (SELECT id FROM species WHERE NAME = 'Digimon') WHERE NAME LIKE '%mon';
+UPDATE animals SET species_id = (SELECT id FROM species WHERE NAME = 'Pokemon') WHERE NAME NOT LIKE '%mon';
+
+-- owner_id
+
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name  = 'Sam Smith') WHERE name IN('Agumon'); 
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name  = 'Jennifer Orwell') WHERE name IN('Gabumon' , 'Pikachu'); 
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name  = 'Bob') WHERE name IN('Devimon', 'Plantmon'); 
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name  = 'Melody Pond ') WHERE name IN('Charmander', 'Squirtle', 'Blossom'); 
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name  = 'Dean Winchester') WHERE name IN('Angemon' , 'Boarmon'); 
--- schema.sql
+++ schema.sql
@@ -1,10 +1,35 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT PRIMARY KEY NOT NULL,
+    id INT GENERATED ALWAYS AS IDENTITY, 
     name VARCHAR(100) NOT NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INT  NOT NULL,
     nutered BOOLEAN NOT NULL,
     weight_kg FLOAT NOT NULL);
 ALTER TABLE animals ADD species VARCHAR(100);
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY, 
+    full_name VARCHAR(255) NOT NULL, 
+    age INT NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY, 
+    name VARCHAR(255) NOT NULL, 
+
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE animals DROP COLUMN species; 
+
+ALTER TABLE animals ADD COLUMN species_id INT; 
+ALTER TABLE animals ADD CONSTRAINT species_fk FOREIGN KEY (species_id) REFERENCES species(id)
+ON DELETE CASCADE;
+
+ALTER TABLE animals ADD COLUMN owner_id INT; 
+ALTER TABLE animals ADD CONSTRAINT owner_fk FOREIGN KEY (owner_id) REFERENCES owners(id)
+ON DELETE CASCADE;
